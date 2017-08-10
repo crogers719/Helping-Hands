bbc599ef7b377a4960567b13300f3b324319086d
Colleen Rogers <crogers719@gmail.com>
2017-08-10 10:22:37 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,63 +1,33 @@
 <!DOCTYPE html>
-
-<?php include "login.php"; ?>  
 <html lang="en">
+<?php include "../database.php" ?>
+<?php include "../connect.php" ?>
+<?php session_start(); ?>	
+<?php include "../_header.php"; ?>
 
-<?php include "_header.php"; ?>  
+
 <div class="container">
-
-	<div class="row">
-	<div class="col-lg-12">
-		<div class="bs-component">
-			<div class="jumbotron">
-			  <h1>Helping Hands Non-Profit Auction</h1>
-			  <p>Welcome to Helping Hands Auction</p>
-			</div>		
-		</div>	
-	</div>
-	
- 	
+	<div class="page-header" id="banner">
 		<div class="row">
-			<div class="col-lg-6">
-				<?php if(!isset($_SESSION['user'])){ ?>
-					<div class="well bs-component">
-						<form action="" method="post" class="form-horizontal">
-							<fieldset>
-								<div class="form-group">
-									<label for="username" class="col-lg-2 control-label">User Name</label>
-									<div class="col-lg-10">
-										<input type="text" class="form-control" id="username" name="username" placeholder="username" value="<?php if(isset($username)){ echo htmlspecialchars($username);} ?>" required>
-									</div>
-								</div>
-								<div class="form-group">
-									<label for="password" class="col-lg-2 control-label">Password</label>
-									<div class="col-lg-10">
-										<input type="text" class="form-control" id="password" name="password" placeholder="Password" value="<?php if(isset($password)){ echo htmlspecialchars($password);} ?>">
-									</div>
-								</div>
-								<div class="col-lg-10 col-lg-offset-2">
-										<a href="forgotpass.php">Forgot Password</a>
-										&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
-										<a href="register.php">Register</a>
-										<br>
-								</div>
-								<div class="form-group">
-									<div class="col-lg-10 col-lg-offset-2">
-										<button type="submit" class="btn btn-primary" name="index.php" onclick="return confirm('You are about to login. Do you want to continue?');">Login</button>
-									</div>
-								</div>
-							</fieldset>
-						</form>
+			<!-- adjusts to window width -->
+			<div class="col-lg-8 col-md-7 col-sm-6">
+				<div class="bs-component">
+					<h1>Welcome back Admin!</h1>
+					<h2>Admininstrative Home Page</h2>
+					<div class="btn-group-vertical">
+
+						<a href="/~CR1501/project_2/admin/active_auctions.php" class="btn btn-default">All Active Auction Items</a>
+						<a href="/~CR1501/project_2/admin/active_items_category.php" class="btn btn-default">Active Items In a Category</a>
+						<a href="/~CR1501/project_2/admin/maintenance/index.php" class="btn btn-default">Maintenance</a>
+						<a href="/~CR1501/project_2/admin/reporting/index.php" class="btn btn-default">Reporting</a>
 					</div>
-				<?php } ?>
-			</div>					
-
+				</div>
+			</div>
 		</div>
-
-
-
+	</div>
 </div>
-
-<?php include "_footer.php"; ?>
+	
+	
+<?php include "../_footer.php"; ?>
 
 </html>
